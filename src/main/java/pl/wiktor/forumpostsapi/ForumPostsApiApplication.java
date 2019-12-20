5d8354aebcd298d70ce7c15ec791dda684a6ffd3
Wiktor Krzyzanowski <wiktor.krzyzanowski1@gmail.com>
2019-12-20 21:04:23 +0100

    init with start post entity

---
+++ src/main/java/pl/wiktor/forumpostsapi/ForumPostsApiApplication.java
@@ -0,0 +1,13 @@
+package pl.wiktor.forumpostsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumPostsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumPostsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/wiktor/forumpostsapi/ForumPostsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wiktor.forumpostsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumPostsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
